bc31455fd89efe0d7f244e30cb0f558f2aefa8a6
rixo <eric@rixo.fr>
2019-07-29 03:55:39 +0200

    move dev server options to webpack.config.js (for better interop with HMR test suite)

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,26 @@ const mode = process.env.NODE_ENV || 'development'
 const prod = mode === 'production'
 const dev = !prod
 
+// Optimistic tries to preserve components' state accross compile / runtime
+// errors, by displaying error placeholders components to prevent full reload.
+//
+// This is obviously a very tricky business, and the app / HMR will end in an
+// undetermined state more often.
+//
+// Yet, the trade off may still be preferable for some people. Your state is
+// preserved for a longer time, in exchange you have to manually hit ctrl-r
+// more often. And also the occasional ctrl-r to clear the doubt: does my app
+// really have a problem, or is that a HMR glitch?
+//
+// Webpack Dev Server's own compile error overlay is not useful when optimistic
+// is ON, because Webpack won't see compile errors as such anymore. Otherwise
+// it's pretty cool in HMR context, so we advise to have one or the other.
+//
+// NOTE Optimistic is not correctly implemented currently in svelte-loader.
+//
+const optimistic = false
+const overlay = !optimistic
+
 module.exports = {
   entry: {
     bundle: ['./src/main.js'],
@@ -60,14 +80,14 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
-    new webpack.HotModuleReplacementPlugin(),
   ],
-  devtool: prod ? false : 'source-map',
-
-  devServer: {
-    hot: true,
-  },
   optimization: {
     minimize: false,
+  },
+  devtool: prod ? false : 'source-map',
+  devServer: {
+    contentBase: 'public',
+    hot: true,
+    overlay,
   },
 }
